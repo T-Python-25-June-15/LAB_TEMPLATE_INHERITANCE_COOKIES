59caa7bc7bb6551ae3ce66da9f3f6168307b7180
Saif <sknalyami@gmail.com>
2025-07-10 02:08:11 +0300

    I finished contact page

--- Booking/main/views.py
+++ Booking/main/views.py
@@ -8,11 +8,9 @@ def home_view (request:HttpRequest):
 def set_mode(request: HttpRequest, mode: str):
     if mode not in ['light', 'dark']:
         mode = 'light'
-    
     response = redirect(request.META.get('HTTP_REFERER', '/'))
     response.set_cookie('mode', mode, max_age=60*60*24*30)
     return response
-
 
 def properties_view (request:HttpRequest):
     return render (request, "main/properties.html")
