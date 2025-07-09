f8bf86919ca88557f0c4c85a08e8b1847ab7d7dc
Saif <sknalyami@gmail.com>
2025-07-09 23:35:02 +0300

    I finished the base and the index

---
+++ Booking/main/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse
+
+
+def home_view (request:HttpRequest):
+    return render(request , "main/index.html")
+
+def set_mode(request: HttpRequest, mode: str):
+    if mode not in ['light', 'dark']:
+        mode = 'light'
+    
+    response = redirect(request.META.get('HTTP_REFERER', '/'))
+    response.set_cookie('mode', mode, max_age=60*60*24*30)
+    return response
+
+
+def properties_view (request:HttpRequest):
+    return render (request, "main/properties.html")
+
+def contact_view (request:HttpRequest):
+    return render (request, "main/contact.html")
+
+
