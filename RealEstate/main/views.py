48c00f4d126c796fd26b771ab4a30daa663beb54
Nawaf-Almalki028 <xp.mo91@gmail.com>
2025-07-09 20:55:23 +0300

    done with property

--- RealEstate/main/views.py
+++ RealEstate/main/views.py
@@ -7,8 +7,15 @@ def home_page(request:HttpRequest):
     return render(request ,"main/index.html", {"theme": theme})
 
 def properties_page(request:HttpRequest):
+    properties = [
+        {"title" : "Villa Modern in Malqa", "image" : "villa1.jpg"},
+        {"title" : "Great home for you in Rimal", "image" : "villa2.jpg"},
+        {"title" : "Villa with 8 bedrooms in Swedey", "image" : "villa3.jpg"},
+        {"title" : "Amazing Villa in Hitten", "image" : "villa4.jpg"},
+    ]
+
     theme = request.COOKIES.get("theme", "light")
-    return render(request ,"main/properties.html", {"theme": theme})
+    return render(request ,"main/properties.html", {"theme": theme,"properties": properties})
 
 def contact_page(request:HttpRequest):
     theme = request.COOKIES.get("theme", "light")
