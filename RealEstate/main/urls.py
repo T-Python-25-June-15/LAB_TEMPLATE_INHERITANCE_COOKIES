fcc52f715d1faa268ad98762a0789d5af2b2b4fc
Abdullah Alsofayan <alsofayan10@gmail.com>
2025-07-09 14:32:55 +0300

    almost finish contact page

--- RealEstate/main/urls.py
+++ RealEstate/main/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 app_name = "main"
 
 urlpatterns = [
-    path('', views.home_page, name="home_page")
+    path('', views.home_page, name="home_page"),
+    path('properties/', views.properties_page, name= 'properties_page'),
+    path('contact/', views.contact_page, name= 'contact_page'),
 ]
--- RealEstate/main/views.py
+++ RealEstate/main/views.py
@@ -5,3 +5,16 @@ from django.http import HttpRequest, HttpResponse
 
 def home_page(request: HttpRequest):
     return render(request, "home_page.html")
+
+def properties_page(request:HttpRequest):
+
+    properties = [
+    {"title" : "Villa Modern in Malqa", "image" : "villa1.jpg"},
+    {"title" : "Great home for you in Rimal", "image" : "villa2.jpg"},
+    {"title" : "Villa with 8 bedrooms in Swedey", "image" : "villa3.jpg"},
+    {"title" : "Amazing Villa in Hitten", "image" : "villa4.jpg"},
+    ]
+    return render(request, "properties.html", context={"properties": properties})
+
+def contact_page(request:HttpRequest):
+    return render(request, "contact.html")
