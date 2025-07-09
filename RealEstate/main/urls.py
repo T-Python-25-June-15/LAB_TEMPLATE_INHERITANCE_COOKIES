b926eb2165f5de13f5ba288355ecd678028b3657
Nawaf-Almalki028 <xp.mo91@gmail.com>
2025-07-09 20:29:48 +0300

    done

--- RealEstate/main/urls.py
+++ RealEstate/main/urls.py
@@ -3,4 +3,9 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home_page, name="home"),
+    path('properties/', views.properties_page, name="properties"),
+    path('contact/', views.contact_page, name="contact"),
+    path('theme/light/', views.to_light_change, name="light"),
+    path('theme/dark/', views.to_dark_change, name="dark"),
+    
 ]
--- RealEstate/main/views.py
+++ RealEstate/main/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse,HttpRequest
 
 
 def home_page(request:HttpRequest):
+    theme = request.COOKIES.get("theme", "light")
+    return render(request ,"main/index.html", {"theme": theme})
 
-    return render(request ,"main/index.html")
+def properties_page(request:HttpRequest):
+    theme = request.COOKIES.get("theme", "light")
+    return render(request ,"main/properties.html", {"theme": theme})
+
+def contact_page(request:HttpRequest):
+    theme = request.COOKIES.get("theme", "light")
+    return render(request ,"main/contact.html", {"theme": theme})
     
+def to_light_change(request:HttpRequest):
+
+    response = redirect("/")
+    response.set_cookie("theme","light",max_age=60*60*24)
+    return response
+
+def to_dark_change(request:HttpRequest):
+
+    response = redirect("/")
+    response.set_cookie("theme","dark",max_age=60*60*24)
+    return response
