b31d982995b073c4a28e6d72a1f121e05f7ebe82
wasan <wasanalqahtani2@gmail.com>
2025-07-09 23:33:06 +0300

    My project Wasan ALqahtani

---
+++ RealEstate/main/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse
+
+def home_view(request : HttpRequest):
+   return render(request, "main/home.html")
+
+
+def properties_view(request : HttpRequest):
+    properties = [
+    {"title" : "Villa Modern in Malqa", "image" : "villa1.jpg"},
+    {"title" : "Great home for you in Rimal", "image" : "villa2.jpg"},
+    {"title" : "Villa with 8 bedrooms in Swedey", "image" : "villa3.jpg"},
+    {"title" : "Amazing Villa in Hitten", "image" : "villa4.jpg"},
+]
+    return render(request, "main/properties.html", context={"properties":properties})
+
+def contact_view(request : HttpRequest):
+    return render(request, "main/contact.html")
+
+def dark_mode_view(request : HttpRequest):
+    referernce = request.META.get('HTTP_REFERER', '/')
+    response = redirect(referernce)
+    response.set_cookie("mode","dark")
+    return response
+
+def light_mode_view(request : HttpRequest):
+    referernce = request.META.get('HTTP_REFERER', '/')
+    response = redirect(referernce)
+    response.set_cookie("mode","light")
+    return response
