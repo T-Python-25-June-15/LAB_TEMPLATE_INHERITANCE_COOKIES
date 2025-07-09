686c472ddf37e951ad793a1043b4db285b219ce1
Saif <sknalyami@gmail.com>
2025-07-10 02:29:45 +0300

    Some fixes

--- Booking/main/views.py
+++ Booking/main/views.py
@@ -13,7 +13,13 @@ def set_mode(request: HttpRequest, mode: str):
     return response
 
 def properties_view (request:HttpRequest):
-    return render (request, "main/properties.html")
+    properties = [
+        {"title": "Villa Modern in Malqa", "image": "villa1.jpg"},
+        {"title": "Great home for you in Rimal", "image": "villa2.jpg"},
+        {"title": "Villa with 8 bedrooms in Swedey", "image": "villa3.jpg"},
+        {"title": "Amazing Villa in Hitten", "image": "villa4.jpg"},
+    ]
+    return render (request, "main/properties.html", context={"properties": properties})
 
 def contact_view (request:HttpRequest):
     return render (request, "main/contact.html")
