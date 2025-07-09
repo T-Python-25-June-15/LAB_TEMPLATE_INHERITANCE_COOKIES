8e8b6b67b5fa90b3e2cc8288840ef346b03a7cb3
Moh0py <1vipmoh@gmail.com>
2025-07-09 22:21:21 +0300

    Compelet

--- RealEstate/main/urls.py
+++ RealEstate/main/urls.py
@@ -1,7 +1,11 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path,include
+from . import views as main 
+
+app_name = 'main'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls')),
+    path('', main.page_view, name='page_view'),
+    path('change_mode', main.change_mode, name='change_mode'),
+    path('contact/', main.contact_view, name='contact_view'),
+    path('properties/', main.properties_view, name='properties_view'),
 ]
--- RealEstate/main/views.py
+++ RealEstate/main/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse
 # Create your views here.
+
+
+def page_view(request:HttpRequest):
+    r = render(request, 'main/homepage.html')
+    if not 'mode' in request.COOKIES:
+        r.set_cookie('mode', 'light', max_age=60*60*24)
+    return r
+
+def contact_view(request:HttpResponse):
+    r = render(request, 'main/contact.html')
+    if not 'mode' in request.COOKIES:
+        r.set_cookie('mode', 'light', max_age=60*60*24)
+    return r
+
+def properties_view(request:HttpResponse):
+    r = render(request, 'main/properties.html')
+    if not 'mode' in request.COOKIES:
+        r.set_cookie('mode', 'light', max_age=60*60*24)
+    return r
+
+def change_mode(request:HttpRequest):
+    r = redirect('/')
+    if 'mode' in request.COOKIES:
+        if request.COOKIES['mode'] == 'dark':
+            r.set_cookie('mode', 'light')
+        else:
+            r.set_cookie('mode', 'dark')
+        
+    else:
+        r.set_cookie('mode', 'light', max_age=60*60*24)
+        
+    return r
+        
+    
