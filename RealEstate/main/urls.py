16d703e8fb7b3611b9e5c78901f526c78fe9d626
NaifM8 <iinaif-@hotmail.com>
2025-07-10 00:48:39 +0300

    completed

---
+++ RealEstate/main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = "main"
+
+urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('properties/', views.properties_view, name='properties'),
+    path('contact/', views.contact_view, name='contact'),
+    path('toggle-theme/', views.toggle_theme, name='toggle-theme'),
+]
