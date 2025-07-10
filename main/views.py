3b7ac107323258c63a1bfa2d2c939e2df9b1cd9d
Turki20 <ttaiealmutairi@stu.kau.edu.sa>
2025-07-10 10:16:03 +0300

    Turki Ahmed

--- main/views.py
+++ main/views.py
@@ -16,7 +16,13 @@ def contact_view(request:HttpResponse):
     return resp
 
 def properties_view(request:HttpResponse):
-    resp = render(request, 'main/properties.html')
+    properties = [
+        {"title" : "Villa Modern in Malqa", "image" : "villa1.jpg"},
+        {"title" : "Great home for you in Rimal", "image" : "villa2.jpg"},
+        {"title" : "Villa with 8 bedrooms in Swedey", "image" : "villa3.jpg"},
+        {"title" : "Amazing Villa in Hitten", "image" : "villa4.jpg"},
+    ]
+    resp = render(request, 'main/properties.html', {"properties": properties})
     if not 'mode' in request.COOKIES:
         resp.set_cookie('mode', 'light', max_age=60*60*24)
     return resp
