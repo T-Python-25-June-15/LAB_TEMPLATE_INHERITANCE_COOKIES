ab2a26826d3e383ab1946e4152d321e09a57ad67
Nasser-Alsuduni0 <Nasser.alsuduni@outlook.sa>
2025-07-10 09:01:41 +0300

    lap solution

---
+++ RealEstate/main/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views as main
+from . import views
+
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', views.home_page, name='home_page'),  
+    path('properties/', views.properties_page, name='properties_page'),
+    path('contact/', views.contact_page, name='contact_page'),
+    path('theme/light/', views.to_light_change, name="light"),
+    path('theme/dark/', views.to_dark_change, name="dark"),
+]
